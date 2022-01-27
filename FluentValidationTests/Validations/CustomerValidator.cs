2ee909782e237ba7af2d4114272da1d23ce2ec7e
Caio Cavallieri <caiomjc@hotmail.com>
2022-01-27 16:57:57 -0300

    Changing discount validation and adding message to it.

--- FluentValidationTests/Validations/CustomerValidator.cs
+++ FluentValidationTests/Validations/CustomerValidator.cs
@@ -9,7 +9,7 @@ namespace FluentValidationTests.Validations
         {
             RuleFor(customer => customer.Id).NotNull().NotEmpty().GreaterThan(0);
             RuleFor(customer => customer.Name).NotNull().NotEmpty();
-            RuleFor(customer => customer.Discount).LessThan(20);
+            RuleFor(customer => customer.Discount).LessThanOrEqualTo(20).WithMessage("Discount can't be greater than 20."); ;
         }
     }
 }
